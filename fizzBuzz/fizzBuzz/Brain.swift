2f42f4b6ec9ca8c84f948ecac008d5b5ba11967e
CrossroadsMan <crossroadsman@gmail.com>
2016-05-14 20:59:01 -0600

    quick help
    just added quick help documentation to the methods in Brain class

--- fizzBuzz/fizzBuzz/Brain.swift
+++ fizzBuzz/fizzBuzz/Brain.swift
@@ -9,11 +9,23 @@
 import Foundation
 
 class Brain {
-    
+    /**
+    Checks if an Int is divisible by another Int with no remainder. Returns true if so.
+     
+     - parameter number: (Int) the element to check if it can be evenly divided by N
+     - parameter N: (Int) (internally n). The divisor
+     - returns: Bool
+     */
     func isDivisibleByN(number: Int, N n: Int) -> Bool {
         return 0 == number % n
     }
     
+    /**
+    Gives the String output for any given Int input
+     
+     - parameter number: (Int) the number to get a fizzbuzz reponse to
+     - returns: String (fizzbuzz if divisible by 3 and 5, fizz if by 3, buzz if by 5, otherwise the input number (but expressed as a String)
+     */
     func response(number: Int) -> String {
         switch number {
         case _ where isDivisibleByN(number, N: 15):
