b795f5ab720c60bc90ffe9d0971a038a160a6abc
CrossroadsMan <crossroadsman@gmail.com>
2016-05-16 00:36:48 -0600

    add view
    added label (3/5)
    added gameplay buttons and start again button
    
    TODO: Link UI to viewcontroller

---
+++ fizzBuzz/fizzBuzzTests/ViewControllerUnitTests.swift
@@ -0,0 +1,25 @@
+//
+//  ViewControllerUnitTests.swift
+//  fizzBuzz
+//
+//  Created by Alex Koumparos on 16/05/16.
+//  Copyright © 2016 Alex Koumparos. All rights reserved.
+//
+
+@testable import fizzBuzz
+import XCTest
+
+class ViewControllerUnitTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    
+}
