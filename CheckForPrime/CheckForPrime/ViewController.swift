5a827d4f1f7f1206521d594c846bf46e77cee06c
CrossroadsMan <crossroadsman@gmail.com>
2016-05-19 18:00:26 -0600

    add checkForPrime
    created new XCode project: CheckForPrime. See: http://rshankar.com/test-driven-development-in-swift/

---
+++ CheckForPrime/CheckForPrime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckForPrime
+//
+//  Created by Alex Koumparos on 19/05/16.
+//  Copyright © 2016 Alex Koumparos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
